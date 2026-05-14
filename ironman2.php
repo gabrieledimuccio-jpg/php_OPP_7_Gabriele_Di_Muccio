6e55d1d84a7f780bdbeff2c8c20999578980f13d
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-05-14 13:19:59 +0200

    creati ironman22.php, ironman33.php

--- ironman2.php
+++ ironman2.php
@@ -1,3 +1,17 @@
 <?php
+abstract class LeftArm{
+abstract public function attack2();
+}
 
+class ScudoMagnetico extends LeftArm{
+public function attack2(){
+echo "Scudo magnetico attivato \n";
+}
+}
+
+class Missili extends LeftArm{
+public function attack2(){
+echo "Missili attivati \n";
+}
+}
 ?>
--- ironman3.php
+++ ironman3.php
@@ -1,3 +1,17 @@
 <?php
+abstract class Legs{
+abstract public function attack3();
+}
 
+class Propulsori extends Legs{
+public function attack3(){
+echo "Propulsori attivati \n";
+}
+}
+
+class Cannoni extends Legs{
+public function attack3(){
+echo "Cannoni attivati \n";
+}
+}
 ?>
